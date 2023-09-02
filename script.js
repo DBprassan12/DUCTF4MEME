1c7d2399f05bdb6e7115a9a59f1ee1270bb76212
DBprassan12 <divij.bhaw15@gmail.com>
2023-09-03 00:21:13 +0800

    aa

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var logo = document.getElementById('logo');
 var button = document.querySelector('button');
 const audioElement = document.getElementById("audio");
 const content = document.querySelector(".container-hidden");
-
+var invisibleElements = document.querySelectorAll('.meme');
 
 
 // Add a click event listener to the button
@@ -12,6 +12,11 @@ button.addEventListener('click', function() {
     logo.style.display = 'none';
     button.style.display = 'none';
     audioElement.play();
+
+
+    for (var i = 0; i < invisibleElements.length; i++) {
+        invisibleElements[i].style.display = 'block';
+      }
 
 
 });
